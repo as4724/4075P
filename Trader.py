ff7e7e780963b5a801892eae4454f5284881bc63
Akshat Sinha <as4724@columbia.edu>
2015-04-19 05:41:48 -0400

    First draft

--- Trader.py
+++ Trader.py
@@ -20,7 +20,7 @@ class Trade:
     data = pd.DataFrame({})
     orderEntry = pd.DataFrame({})
     orderExit = pd.DataFrame({})
-    openOrder = pd.DataFrame({})
+    orderOpen = pd.DataFrame({})
     
     def __init__(self, data, chnLen, stopPct):
         self.data = data
@@ -28,7 +28,7 @@ class Trade:
         self.stopPct = stopPct
         self.orderEntry = pd.DataFrame(index = np.arange(0,data.size), columns=('datein','timein','qty','pricein','status'))
         self.orderExit = pd.DataFrame(index = np.arange(0,data.size), columns=('dateout','timeout','qty','priceout','position'))
-        self.openOrder = pd.DataFrame(index = np.arange(0,data.size), columns=('date','time','qty','price','type'))
+        self.orderOpen = pd.DataFrame(index = np.arange(0,data.size), columns=('date','time','qty','price','type'))
         
     def setParam(self, chnLen, stopPct):
         self.chnLen = chnLen
@@ -38,32 +38,55 @@ class Trade:
         self.data = data
         self.orderEntry = pd.DataFrame(index = np.arange(0,data.size), columns=('datein','timein','qty','pricein','status'))
         self.orderExit = pd.DataFrame(index = np.arange(0,data.size), columns=('dateout','timeout','qty','priceout','position'))
-        self.openOrder = pd.DataFrame(index = np.arange(0,data.size), columns=('date','time','qty','price','type'))
+        self.orderOpen = pd.DataFrame(index = np.arange(0,data.size), columns=('date','time','qty','price','type'))
         
     def startTrade(self):
         index = self.chnLen
         i = 0
+        ordEntryInd = 0
         entryPrice = 0
-        for d in self.data[self.chnLen:]:
-            #Process open orders            
-            openBuy = self.openOrder[self.openOrder['qty'] > 0]
-            openSell = self.openOrder[self.openOrder['qty'] < 0]
-            
+        for temp, d in self.data[self.chnLen:].iterrows():
+            #Process open orders 
+            #close open positions
+            for temp2,op in self.orderOpen.iterrows():
+                #Close all open positions in orderEntry and put record in orderExit
+                openPos = self.orderEntry[self.orderEntry['status'] == 'open']
+                marketPos = openPos.qty.sum()
+                for ind, do in openPos.iterrows():
+                    self.orderExit[ind] = [d.Date, d.Time, -self.orderEntry.qty[ind],op.price,marketPos]
+                    marketPos = marketPos - self.orderEntry.qty[ind]
+                    self.orderEntry.status[ind] = 'close'
+                #Add new order position in orderEntry
+                
+                self.orderEntry.loc[ordEntryInd] = [d.Date, d.Time, op.qty,op.price,'open']
+                ordEntryInd += 1
+                #delete entry in orderOpen
+                self.orderOpen.drop(temp2)
+                
+            print '======Order Open======'
+            print self.orderOpen
+            print '======Order Entry======'
+            print self.orderEntry
+            print '======Order Exit======='
+            print self.orderExit
+    
             #Create new orders
             hhigh = self.data.High[self.data.High[(index-self.chnLen):index]]
             llow = self.data.Low[self.data.Low[(index-self.chnLen):index]]
             if self.marketPos == 0:
-                self.openOrder[i] = [d.Date, d.Time, self.tradeSize, hhigh, 'stop']
+                self.orderOpen.loc[i] = [d.Date, d.Time, self.tradeSize, hhigh, 'stop']
                 i += 1
-                self.openOrder[i] = [d.Date, d.Time, -self.tradeSize, llow, 'stop']
+                self.orderOpen.loc[i] = [d.Date, d.Time, -self.tradeSize, llow, 'stop']
                 i += 1
             elif(self.marketPos > 0):
                 prevPeak = entryPrice
                 if d.Close > prevPeak:
                     prevPeak = d.Close
-                self.openOrder[i] = [d.Date, d.Time, -self.tradeSize, prevPeak * (1-self.stopPct), 'stop']
+                self.orderOpen.loc[i] = [d.Date, d.Time, -self.tradeSize, prevPeak * (1-self.stopPct), 'stop']
+                i += 1
             else:
                 prevTrough = entryPrice
                 if d.Close < prevTrough:
                     prevTrough = d.Close
-                self.openOrder[i] = [d.Date, d.Time, self.tradeSize, prevTrough * (1+self.stopPct), 'stop']
+                self.orderOpen.loc[i] = [d.Date, d.Time, self.tradeSize, prevTrough * (1+self.stopPct), 'stop']
+                i += 1
